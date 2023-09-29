d97cf3997b00ec8a29daa989022b39cab3647321
korol-galyna <galyna.korol@gmail.com>
2023-09-29 14:53:01 +0300

    29.09.2023 update

---
+++ js/01-gallery.js
@@ -0,0 +1,54 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+console.log(galleryItems);
+
+/*створення і рендер розмітки на підставі масиву даних galleryItems*/
+
+const galleryList = document.querySelector(".gallery");
+galleryList.insertAdjacentHTML("beforeend", createMarkup(galleryItems));
+
+function createMarkup (arr){
+return arr
+.map(
+({preview,original,description}) => 
+
+`<li class="gallery__item">
+<a class="gallery__link" href="${original}">
+  <img
+    class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"
+  />
+</a>
+</li>`
+)
+    .join("");
+}
+
+/*Реалізація делегування на ul.gallery і отримання url великого зображення.*/ 
+
+galleryList.addEventListener("click", handleGalleryClick);
+
+function handleGalleryClick(event) {
+    event.preventDefault(); 
+
+    const target = event.target;
+    if (target.nodeName !== "IMG") {
+        return;
+    }
+    
+    const largeImageURL = target.dataset.source;
+    openModalWithImage(largeImageURL);
+  }
+  
+  function openModalWithImage(imageURL) {
+    
+    const instance = basicLightbox.create(`
+      <img src="${imageURL}" alt="Image" />
+    `);
+  
+    instance.show();
+  }
+
+  
